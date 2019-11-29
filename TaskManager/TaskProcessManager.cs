86f6a2b5dd2da7d1752f40aad0c45a96b3cb05bd
DinaraZZ <dinara.rakhymbekova@gmail.com>
2019-11-29 21:16:53 +0600

    Add project files.

---
+++ TaskManager/TaskProcessManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace TaskManager
+{
+    public static class TaskProcessManager
+    {
+        public static List<Process> GetListProcesses()
+        {
+            List<Process> processesToReturn = new List<Process>();
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                processesToReturn.Add(process);
+            }
+
+            return processesToReturn;
+        }
+
+        public static bool DeleteProcess(int Id)
+        {
+            try
+            {
+                Process.GetProcessById(Id).Kill();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
